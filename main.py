3fa56fda22df9e1758003441f86f40799afea5ae
dmd404 <dpfj484@gmail.com>
2024-01-17 20:27:19 +0900

    feat: define print_slot_machine()
    transpose so it can represent vertial rows

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+# symbols in a wheel/COLS
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+# outcome of slot machine
+# generate items in slot machine
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    
+    columns = []
+    # generate a column for every column there is
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]  # copy all_symbols 
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+    
+    return columns
+
+
+def print_slot_machine(columns):
+    # transpose
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:  # maximum index to have access to shark film
+                print(column[row], "|")
+            else:
+                print(column[row])
+
 
 
 def deposit():
