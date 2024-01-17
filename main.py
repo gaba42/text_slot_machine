43b3b6ac817fee8283e72242ad16fc1ec257caa3
dmd404 <dpfj484@gmail.com>
2024-01-17 20:45:56 +0900

    feat: prettify slot machine

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ symbol_count = {
     "A": 2,
     "B": 4,
     "C": 6,
-    "D": 8,
+    "D": 8, 
 }
 
 # outcome of slot machine
@@ -44,9 +44,11 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:  # maximum index to have access to shark film
-                print(column[row], "|")
+                print(column[row],end=" | ")
             else:
-                print(column[row])
+                print(column[row], end="")
+        
+        print()
 
 
 
@@ -114,6 +116,9 @@ def main():
         else:
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 
